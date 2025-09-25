666dc662738ed866e26c359ddfc5a8e5741048d4
extremeremixboyz <shahingorginziarati@icloud.com>
2025-09-25 19:30:09 +0200

    MyQueue

---
+++ Advanced.NET/MyQueue/MyQueue/MyQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    internal class MyQueue <T>
+    {
+        private List<T> _list = new List<T> ();
+
+        public MyQueue ()
+        {
+            //_list = new List<T> ();
+        }
+
+        public void Add (T item)
+        {
+            _list.Add (item);
+        }
+
+        public T? Remove ()
+        {
+            if (_list.Count != 0)
+            {
+                T item = _list[0];
+                _list.RemoveAt (0);
+                return item;
+            }
+            return default;
+            
+            
+        }
+
+        public T? peek()
+        {
+            if (_list.Count != 0)
+            {
+                T item = _list[0];
+                return item;
+            }
+            return default;
+        }
+
+        
+    }
+}
---
+++ Advanced.NET/MyQueue/MyQueue/Program.cs
@@ -0,0 +1,8 @@
+﻿using MyQueue;
+
+MyQueue<int> integerQueue = new MyQueue<int> ();
+integerQueue.Add (1);
+integerQueue.Add (2);
+integerQueue.Add (3);
+Console.WriteLine (integerQueue.peek ());
+
